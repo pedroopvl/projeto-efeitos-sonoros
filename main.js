ffcd744cec1cf92784cbdd4128fae30b992ba757
Pedro Paulo <pedropottergd@gmail.com>
2025-03-17 13:30:23 -0300

    removendo comentarios

--- main.js
+++ main.js
@@ -6,17 +6,15 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-for (let contador = 0; contador < ListaDeTeclas.length) {
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
 
     const tecla = ListaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     //template string
     const idAudio = `#som_${instrumento}`;
 
-    //console.log(idAudio);
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
     contador = contador + 1;
-    //console.log(contador);
 }
